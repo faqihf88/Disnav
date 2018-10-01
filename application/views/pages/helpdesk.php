255f092c01de182a1f72c8f4fe52baeeac1aabdf
anantakatta <hansieananta@gmail.com>
2018-10-01 14:39:09 +0700

    helpdesk
    ngewe

--- application/views/pages/helpdesk.php
+++ application/views/pages/helpdesk.php
@@ -21,7 +21,7 @@
           <div class="card-body">
             <form action="<?php echo base_url(). 'helpdesk/simpan'; ?>" method="post">>
   <div class="form-group row">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Nama</label>
     <div class="col-sm-10">
       <input type="email" class="form-control" name="nama" id="inputEmail3" placeholder="Input Your name">
     </div>

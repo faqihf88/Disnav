189887a77a93db3f945aa7fd3cb0d6fd4d293fee
faqihf88 <faqihf88@gmail.com>
2018-10-02 16:34:14 +0700

    fix tampilan surat telkompel
    blablabla

--- application/views/pages/sarpras/telkompel.php
+++ application/views/pages/sarpras/telkompel.php
@@ -19,7 +19,183 @@
       <div class="col-12">
         <div class="card">
           <div class="card-body">
-            This is some text within a card block.
+            <h4 class="card-title" align="center">LAPORAN HARIAN SROP CIREBON</h4>
+            <h4 class="card-title" align="center">DISTRIK NAVIGASI KELAS I TANJUNG PRIOK</h4>
+            <br><br>
+            <form action="#" class="form-horizontal form-bordered">
+              <div class="form-body">
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">HARI</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control" placeholder="Hari">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">TANGGAL</label>
+                  <div class="col-md-3">
+                    <input type="date" class="form-control" placeholder="dd/mm/yyyy">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">WAKTU</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control" placeholder="Jam">
+                  </div>
+                </div>
+
+                <h5 class="card-title">I. Lalu lintas Kapal</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kapal Masuk</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kapal Keluar</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kapal Melintas</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kapal Labuh</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kapal Sandar</label>
+                  <div class="col-md-3">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+
+                <h5 class="card-title">II. Prakiraan Cuaca Wilayah Pelabuhan dari BMKG</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Cuaca</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Arah Angin</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Kecepatan Angin</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Jarak Pandang</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Tinggi Gelombang</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Suhu Udara Maximum</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Suhu Udara Minimum</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Pasang Surut Maximum</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+
+                <h5 class="card-title">III. Kondisi</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Alur Pelayaran</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">SBNP</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Peralatan</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>          
+
+                <h5 class="card-title">IV. Jasa PNBP VTS/Telegram Radio</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Nota Tagih</label>
+                  <div class="col-md-2">
+                    <input type="text" class="form-control">
+                  </div>
+                </div> 
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Master Cable</label>
+                  <div class="col-md-6">
+                    <input type="text" class="form-control">
+                  </div>
+                </div> 
+
+                <h5 class="card-title">V. Lain - lain</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Lain - lain</label>
+                  <div class="col-md-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+
+                <h5 class="card-title">VI. Petugas Jaga</h5><br>
+                <div class="form-group row">
+                  <label class="control-label text-left col-md-3">Petugas 1</label>
+                  <div class="col-md-6">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group row last">
+                  <label class="control-label text-left col-md-3">Petugas 2</label>
+                  <div class="col-md-6">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+              </div>
+              <div class="form-actions">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="row">
+                      <div class="offset-sm-3 col-md-9">
+                        <button type="submit" class="btn btn-success">
+                          <i class="fa fa-check"></i>Submit
+                        </button>
+                        <button type="button" class="btn btn-inverse">Cancel</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
